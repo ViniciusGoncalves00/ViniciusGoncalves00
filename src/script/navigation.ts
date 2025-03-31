6b88a461278f5510a78b3cdb8a9eb2b8ead6b5ae
Vinicius <viniciusgoncalves2000@hotmail.com>
2025-03-30 22:49:53 -0300

    nagivation bar

--- src/script/navigation.ts
+++ src/script/navigation.ts
@@ -10,63 +10,55 @@ export class Navigation {
         this._document = document;
 
         this._document.addEventListener('alpine:init', () => {
-            this.ListenWheel()
-            this.ListenSections()
-        })
+            this.ListenWheel();
+            this.ListenSections();
+        });
     }
 
     private ListenWheel() {
         this._window.addEventListener("wheel", (event) => {
-          event.preventDefault();
-        
-          const viewportHeight = this._window.innerHeight;
-          const scrollAmount = viewportHeight * this._rollingWeight;
-        
-          if (event.deltaY > 0) {
-            this._window.scrollTo({
-              top: this._window.scrollY + scrollAmount,
-              behavior: "smooth",
-            });
-          } else {
-            this._window.scrollTo({
-              top: this._window.scrollY - scrollAmount,
-              behavior: "smooth",
-            });
-          }
-        }, { passive: false });
-      }
+            event.preventDefault();
 
-      private ListenSections() {
+            const viewportHeight = this._window.innerHeight;
+            const scrollAmount = viewportHeight * this._rollingWeight;
+
+            this._window.scrollTo({
+                top: this._window.scrollY + (event.deltaY > 0 ? scrollAmount : -scrollAmount),
+                behavior: "smooth",
+            });
+        }, { passive: false });
+    }
+
+    private ListenSections() {
         const observer = new MutationObserver(() => {
             this.highlightCurrentSection();
         });
-      
-        const element = this._document.getElementById('content')
-        if(element) {
-          observer.observe((element), { childList: true, subtree: true });
-        }
-      
-        this._window.addEventListener('load', this.highlightCurrentSection);
-        this._window.addEventListener('scroll', this.highlightCurrentSection);
-      }
 
-      private highlightCurrentSection() {
-        const menuLinks = this._document.querySelectorAll('.menu-link');
+        const element = this._document.getElementById('body');
+        if (element) {
+            observer.observe(element, { childList: true, subtree: true });
+        }
+
+        this._window.addEventListener('load', this.highlightCurrentSection.bind(this));
+        this._window.addEventListener('scroll', this.highlightCurrentSection.bind(this));
+    }
+
+    private highlightCurrentSection() {
+        const navigationOptions = this._document.querySelectorAll('.navigation-option');
         const sections = this._document.querySelectorAll('section');
 
         const viewportHeight = this._window.innerHeight;
         const scrollPosition = this._window.scrollY;
         const windowCenter = scrollPosition + viewportHeight / 2;
-  
+
         sections.forEach((section, index) => {
             const sectionTop = section.offsetTop;
             const sectionBottom = sectionTop + viewportHeight;
-  
+
             if (windowCenter >= sectionTop - this._pixelsTolerance && windowCenter < sectionBottom - this._pixelsTolerance) {
-                menuLinks.forEach((link) => link.classList.remove('bg-red-500'));
-                menuLinks.forEach((link) => link.classList.add('bg-black'));
-                menuLinks[index].classList.remove('bg-black');
-                menuLinks[index].classList.add('bg-red-500');
+                navigationOptions.forEach((link) => link.classList.remove('opacity-100'));
+
+                navigationOptions[index].classList.add('opacity-100');
             }
         });
     }
