feed85da3d13c2333840b795d53495bd1fb5f40b
Vinicius <viniciusgoncalves2000@hotmail.com>
2025-05-01 10:38:22 -0300

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.ts',
@@ -23,6 +25,16 @@ module.exports = {
   {
       extensions: [".tsx", ".ts", ".js"],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/common/icons', to: 'common/icons' },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/templates/index.html',
+    }),
+  ],
   
   mode: 'development',
 };
