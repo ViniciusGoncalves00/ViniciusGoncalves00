19f650b324bc3004376c64d8c9e43b0493ea9025
Vinicius <viniciusgoncalves2000@hotmail.com>
2026-01-17 02:19:38 -0300

    added icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -7,5 +8,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [icon()],
 });
